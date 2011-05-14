rubygems/rubygems

Add 'gem env platform' support.
Closes Feature Request #28300

lib/rubygems/commands/environment_command.rb
@@ -13,6 +13,7 @@ class Gem::Commands::EnvironmentCommand < Gem::Command
           gempath         display path used to search for gems
           version         display the gem format version
           remotesources   display the remote gem servers
+          platform        display the supporte gem platforms
           <omitted>       display everything
     EOF
     return args.gsub(/^\s+/, '')
@@ -76,6 +77,8 @@ lib/rubygems/defaults/operating_system.rb
       out << Gem.path.join(File::PATH_SEPARATOR)
     when /^remotesources/ then
       out << Gem.sources.join("\n")
+    when /^platform/ then
+      out << Gem.platforms.join(File::PATH_SEPARATOR)
     when nil then
       out = "RubyGems Environment:\n"
 

slimgems/slimgems

lib/rubygems/commands/environment_command.rb
@@ -13,6 +13,7 @@ class Gem::Commands::EnvironmentCommand < Gem::Command
           gempath         display path used to search for gems
           version         display the gem format version
           remotesources   display the remote gem servers
+          platform        display the supported gem platforms
           <omitted>       display everything
     EOF
     return args.gsub(/^\s+/, '')
@@ -76,6 +77,8 @@ lib/rubygems/defaults/operating_system.rb
       out << Gem.path.join(File::PATH_SEPARATOR)
     when /^remotesources/ then
       out << Gem.sources.join("\n")
+    when /^platform/ then
+      out << Gem.platforms.join(File::PATH_SEPARATOR)
     when nil then
       out = "RubyGems Environment:\n"
 