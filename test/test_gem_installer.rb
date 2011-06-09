rubygems/rubygems

Add custom_shebang gemrc option for shebang alteration

test/rubygems/test_gem_installer.rb
@@ -18,8 +18,14 @@ class TestGemInstaller < Gem::InstallerTestCase
       @user_installer.spec = @user_spec
       @user_installer.gem_home = @installer_tmp
     end
+
+    @config = Gem.configuration
   end
 
+  def teardown
+    super
+    Gem.configuration = @config
+  end
 
   def test_app_script_text
     @spec.version = 2
@@ -992,6 +998,45 @@ load Gem.bin_path('a', 'executable', version)
     shebang = @installer.shebang 'executable'
 
     assert_equal "#!#{Gem.ruby} -ws", shebang
+  end
+
+  def test_shebang_custom
+    conf = Gem::ConfigFile.new []
+    conf[:custom_shebang] = 'test'
+
+    Gem.configuration = conf
+
+    util_make_exec @spec, "#!/usr/bin/ruby"
+
+    shebang = @installer.shebang 'executable'
+
+    assert_equal "#!test", shebang
+  end
+
+  def test_shebang_custom_with_expands
+    conf = Gem::ConfigFile.new []
+    conf[:custom_shebang] = '1 $env 2 $ruby 3 $exec 4 $name'
+
+    Gem.configuration = conf
+
+    util_make_exec @spec, "#!/usr/bin/ruby"
+
+    shebang = @installer.shebang 'executable'
+
+    assert_equal "#!1 /usr/bin/env 2 #{Gem.ruby} 3 executable 4 a", shebang
+  end
+
+  def test_shebang_custom_with_expands_and_arguments
+    conf = Gem::ConfigFile.new []
+    conf[:custom_shebang] = '1 $env 2 $ruby 3 $exec'
+
+    Gem.configuration = conf
+
+    util_make_exec @spec, "#!/usr/bin/ruby -ws"
+
+    shebang = @installer.shebang 'executable'
+
+    assert_equal "#!1 /usr/bin/env 2 #{Gem.ruby} -ws 3 executable", shebang
   end
 
   def test_unpack

slimgems/slimgems

test/test_gem_installer.rb
@@ -2,6 +2,16 @@ require File.expand_path('../gem_installer_test_case', __FILE__)
 
 class TestGemInstaller < GemInstallerTestCase
 
+  def setup
+    super
+    @config = Gem.configuration
+  end
+
+  def teardown
+    super
+    Gem.configuration = @config
+  end
+
   def test_app_script_text
     util_make_exec '2', ''
 
@@ -815,6 +825,45 @@ load Gem.bin_path('a', 'my_exec', version)
     shebang = @installer.shebang 'my_exec'
 
     assert_equal "#!#{Gem.ruby} -ws", shebang
+  end
+
+  def test_shebang_custom
+    conf = Gem::ConfigFile.new []
+    conf[:custom_shebang] = 'test'
+
+    Gem.configuration = conf
+
+    util_make_exec '2', "#!/usr/bin/ruby"
+
+    shebang = @installer.shebang 'my_exec'
+
+    assert_equal "#!test", shebang
+  end
+
+  def test_shebang_custom_with_expands
+    conf = Gem::ConfigFile.new []
+    conf[:custom_shebang] = '1 $env 2 $ruby 3 $exec 4 $name'
+
+    Gem.configuration = conf
+
+    util_make_exec '2', "#!/usr/bin/ruby"
+
+    shebang = @installer.shebang 'my_exec'
+
+    assert_equal "#!1 /usr/bin/env 2 #{Gem.ruby} 3 my_exec 4 a", shebang
+  end
+
+  def test_shebang_custom_with_expands_and_arguments
+    conf = Gem::ConfigFile.new []
+    conf[:custom_shebang] = '1 $env 2 $ruby 3 $exec'
+
+    Gem.configuration = conf
+
+    util_make_exec '2', "#!/usr/bin/ruby -ws"
+
+    shebang = @installer.shebang 'my_exec'
+
+    assert_equal "#!1 /usr/bin/env 2 #{Gem.ruby} -ws 3 my_exec", shebang
   end
 
   def test_unpack